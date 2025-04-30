95fca90f7a1712f2e28490248e297bcff3ba8581
Vladislav Klimenko <vlad.klimenko.mmv@bk.ru>
2025-04-30 22:16:23 +0300

    add several new languages & update .gitignore file

---
+++ Repositories/jupyternotebook_repos.py
@@ -0,0 +1,23 @@
+import requests
+
+# Creating an API call and saving the response.
+url = 'https://api.github.com/search/repositories?q=language:Jupyter+Notebook&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f'Status code: {r.status_code}')
+
+# Saving the API response in a variable
+response_dict = r.json()
+print(f'Total repositories: {response_dict["total_count"]}')
+
+# Analysis of information about repositories
+repo_dicts = response_dict['items']
+print(f'Repositories returned: {len(repo_dicts)}')
+
+print('\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+    print(f'Name: {repo_dict["name"]}')
+    print(f'Owner: {repo_dict["owner"]["login"]}')
+    print(f'Stars: {repo_dict["stargazers_count"]}')
+    print(f'Repository: {repo_dict["html_url"]}')
+    print(f'Description: {repo_dict["description"]}')
